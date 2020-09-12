cd8a7c7523b1b9c3a7dd2d3dc5ca38dedc341451
Criss-Wang <djj87018507@gmail.com>
2020-09-13 01:16:21 +0800

    Fix coding style issue

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Initializes Remark object.
+     *
+     * @param remark from user input.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
